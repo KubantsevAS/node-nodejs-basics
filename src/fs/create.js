a7e1acb7afe5cc19f824d31fdb8f53a15f6067a4
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 17:15:22 +0300

    create.js: set writeFile as async

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,22 +1,17 @@
-import { existsSync, writeFile } from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import fs from 'node:fs';
 import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, 'files', 'fresh.txt');
 
 const create = async () => {
-    if (existsSync(filePath)) {
+    if (fs.existsSync(filePath)) {
         throw new Error('FS operation failed');
     }
 
-    writeFile(filePath, 'I am fresh and young', error => {
-        if (error) {
-            throw error;
-        }
-    });
+    await fs.promises.writeFile(filePath, 'I am fresh and young');
 };
 
 process.on('uncaughtException', error => {
