64b7b266bd611e1fcb469605d29186309adaaf0b
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 21:48:28 +0300

    esm.mjs: rewrite in ECMAScript notation

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,21 +1,20 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import { createServer as createServerHttp } from 'http';
+import { release, version } from 'os';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-const random = Math.random();
+import aJson from './files/a.json' with { type: 'json' };
+import bJson from './files/b.json' with { type: 'json' };
+import './files/c.cjs';
 
-let unknownObject;
-
-if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
-}
+const unknownObject =  Math.random() > 0.5 ? aJson : bJson;
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
@@ -33,8 +32,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export default {
     unknownObject,
     myServer,
 };
-
