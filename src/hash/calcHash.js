90a0d79a270942f5ab0f067baf5ee2979edf4d6f
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-28 00:53:45 +0300

    calcHash.js: calc sha256 for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const calcHashFile = await fs.promises.open(filePath);
+    const readStream = calcHashFile.createReadStream();
+    const hash = crypto.createHash('sha256');
+
+    readStream.on('data', chunk => hash.update(chunk));
+    readStream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+    readStream.on('error', error => console.error(error));
 };
 
 await calculateHash();
