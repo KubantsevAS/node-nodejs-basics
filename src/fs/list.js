9e9384b54f00cc38799895a33d0ff71b470a16cc
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 17:48:53 +0300

    list.js: implement printing files array function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesDir = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    if (!fs.existsSync(filesDir)) {
+        throw new Error('FS operation failed');
+    }
+
+    const files = await fs.promises.readdir(filesDir);
+    console.log(files);
 };
+
+process.on('uncaughtException', error => {
+    console.error(error);
+    process.exit(1);
+});
 
 await list();
