78ac3f183da5f2682cb14c952bf23a4414b8d542
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 17:29:53 +0300

    rename.js: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const wrongFileDir = path.join(__dirname, 'files/wrongFilename.txt');
+const properFileDir = path.join(__dirname, 'files/properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    if (!fs.existsSync(wrongFileDir) || fs.existsSync(properFileDir)) {
+        throw new Error('FS operation failed');
+    }
+
+    await fs.promises.rename(wrongFileDir, properFileDir);
 };
+
+process.on('uncaughtException', error => {
+    console.error(error);
+    process.exit(1);
+});
 
 await rename();
