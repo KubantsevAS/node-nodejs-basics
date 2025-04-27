5b0c235fe10dddf368815ff96a2166ef282dc37e
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 17:12:36 +0300

    copy.js: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filesDir = path.join(__dirname, 'files');
+const filesCopyDir = path.join(__dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    if (!fs.existsSync(filesDir) || fs.existsSync(filesCopyDir)) {
+        throw new Error('FS operation failed');
+    }
+
+    await fs.promises.cp(filesDir, filesCopyDir, { recursive: true });
 };
+
+process.on('uncaughtException', error => {
+    console.error(error);
+    process.exit(1);
+});
 
 await copy();
