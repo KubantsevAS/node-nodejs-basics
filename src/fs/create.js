0767840042b0b3b74ee8672f934af3b68a5cfa97
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 16:20:26 +0300

    create.js: complete function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { existsSync, writeFile } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    if (existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    writeFile(filePath, 'I am fresh and young', error => {
+        if (error) {
+            throw error;
+        }
+    });
 };
 
 await create();
