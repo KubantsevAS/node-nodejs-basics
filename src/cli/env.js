3fc09db231d15009dfc5d90b80475cf57655af4d
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 20:18:59 +0300

    env.js: implement rss variables logging

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const PREFIX = 'RSS_';
+    const envVariables = process.env;
+
+    const parsedVariables = Object.entries(envVariables)
+        .filter(([key]) => key.startsWith(PREFIX))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    if (parsedVariables.length) {
+        console.log(parsedVariables);
+    }
 };
 
 parseEnv();
