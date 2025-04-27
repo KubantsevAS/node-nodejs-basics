f2d35b7de3c347fec8aeb88f8b52935dc169230c
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-27 21:29:11 +0300

    args.js: implement parse of command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+    const PREFIX = '--';
+
+    const parsedArguments = process.argv
+        .map((value, index, array) => {
+            if (!value.startsWith(PREFIX)) {
+                return;
+            }
+
+            const argValue = !array[index + 1]?.startsWith(PREFIX) ? array[index + 1] : undefined;
+
+            return `${value.replace(PREFIX, '')} is ${argValue}`;
+        })
+        .filter(Boolean)
+        .join(', ');
+
+    console.log(parsedArguments);
 };
 
 parseArgs();
