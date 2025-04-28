8e31289eaa3f6cdd162d9b4e1bee5003c8eb5d93
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-28 15:51:21 +0300

    read.js: implement read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = await fs.promises.open(filePath);
+    const readStream = fileToRead.createReadStream();
+
+    readStream.on('data', chunk => {
+        process.stdout.write(chunk);
+    });
+    readStream.on('end', () => console.log(process.stdout));
+    readStream.on('error', error => console.error(error));
 };
 
 await read();
