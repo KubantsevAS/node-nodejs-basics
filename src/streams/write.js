95a983d78bdbb015dbad659ba93e78b673d88b03
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-28 16:54:58 +0300

    write.js: implement write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,30 @@
+// implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream';
+import path from 'path';
+import fs from 'fs';
+import assert from 'assert';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+console.log(filePath);
+
 const write = async () => {
-    // Write your code here 
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    }
+
+    const fileToWrite = await fs.promises.open(filePath, 'w');
+    const writeStream = fileToWrite.createWriteStream();
+    process.stdin.pipe(writeStream);
 };
+
+process.on('uncaughtException', error => {
+    console.error(error);
+    process.exit(1);
+});
 
 await write();
