085f4a1492a60d8c5c8e49c16f6ea7ac0f5d9f6a
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-28 23:50:18 +0300

    cp.js: implement function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptPath = path.join(__dirname, 'files', 'script.js');
+
+const spawnChildProcess = async args => {
+    const childProcess = spawn(
+        'node',
+        [scriptPath].concat(args),
+        { stdio: ['pipe', 'pipe', 'inherit'] },
+    );
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
