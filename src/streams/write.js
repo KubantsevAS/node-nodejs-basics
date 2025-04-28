d48c4f4d2f4e8eec9a6e8e0e679f07c4a3e7dc3f
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-04-28 16:59:02 +0300

    write.js: refactor

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,10 +1,6 @@
-// implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
-
 import { fileURLToPath } from 'url';
-import { pipeline } from 'stream';
 import path from 'path';
 import fs from 'fs';
-import assert from 'assert';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
